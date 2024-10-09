ada1d7597dad94894c2055f43d99e6fa4d8541e9
Amrita <amritak.saw@gmail.com>
2024-10-09 16:46:04 -0400

    practice complete

---
+++ index.js
@@ -0,0 +1,48 @@
+// const mainTitle = document.querySelector('#main-title')
+// mainTitle.textContent = "Really Awesome Good Movies"
+
+// // Create Nav Bar
+// const navbar = document.createElement("div")
+// navbar.id = navbar
+// const body = document.querySelector('body')
+// body.prepend(navbar)
+
+// const buttonOne = document.createElement('button')
+// buttonOne.textContent = "actors"
+// navbar.append(buttonOne)
+
+// const buttonTwo = document.createElement('button')
+// buttonTwo.textContent = "cast"
+// navbar.append(buttonTwo)
+
+// const buttonThree = document.createElement('button')
+// buttonThree.textContent = "team"
+// navbar.append(buttonThree)
+
+// const buttonFour = document.createElement('button')
+// buttonFour.textContent = "characters"
+// navbar.append(buttonFour)
+
+
+// function disableButton(event) {
+//     event.target.disabled = true
+// }
+
+// buttonOne.addEventListener('click', disableButton)
+// buttonTwo.addEventListener('click', disableButton)
+// buttonThree.addEventListener('click', disableButton)
+// buttonFour.addEventListener('click', disableButton)
+
+// navbar.style.display = 'flex'
+// navbar.style.justifyContent = 'space-between'
+
+// //this is probably how i will make buttons for my website
+// // link is a containedr for the image. not the other way around 
+// // becuase then the link doesn't wrap itself around the whole image it will just be somewhere inside the image (location unknown)
+// const linkToIMDB = document.createElement('a')
+// linkToIMDB.href = 'https://www.imdb.com/title/tt1099212/'
+// navbar.append(linkToIMDB)
+
+// const linkImage = document.createElement('img')
+// linkImage.src = "https://static.wikia.nocookie.net/twilightsaga/images/a/a4/Bella-306318_429619423747956_93621998_n.jpg/revision/latest?cb=20130824013830"
+// linkToIMDB.append(linkImage)
--- practice.js
+++ practice.js
@@ -5,30 +5,93 @@
 
 // // DELIVERABLES // //
 
-// 1. create a new div with an id of .movie-posters and append it to the body
+// 1. create a new div with an id of movie-posters and append it to the body
+const body = document.querySelector('body')
+const moviePosterOne = document.createElement("div")
+moviePosterOne.id = "movie-poster-one"
+body.append(moviePosterOne)
+
 
 // 2. do this three times:
+const moviePosterTwo = document.createElement("div")
+moviePosterTwo.id = "movie-poster-two"
+body.append(moviePosterTwo)
 
-// 2.1 find a movie poster you like and add it as an <img> inside the .movie-poster div
+const moviePosterThree = document.createElement("div")
+moviePosterThree.id = "movie-poster-three"
+body.append(moviePosterThree)
 
-// 2.2 beneath each <img> add the movie title as an <h2>
+// 2.1 find a movie poster you like and add it as an <img> inside the movie-posters div
+const marcel = document.createElement('img')
+marcel.src = "https://image.tmdb.org/t/p/original/q3pOUMWRncUkGJu8J1cYFUS4eKM.jpg"
+moviePosterOne.append(marcel)
+    // marcel.style.width(100)
+    // marcel.style.height(200)
 
-// 2.3 further refine the movie posters:
-// each movie poster gets its own <div> which includes the poster <img>, the title <h2>
-// and a short description <p>
+const marcelTitle = document.createElement('h2')
+marcelTitle.textContent = "Marcel the Shell with Shoes On"
+moviePosterOne.prepend(marcelTitle)
 
-// 4. console.log the title and description for the first movie poster?
+const marcelText = document.createElement('p')
+marcelText.textContent = "Marcel the Shell with Shoes On is a really good movie. You will cry."
+moviePosterOne.append(marcelText)
 
-// 5. change the <img> src, title text, and description text for the second movie poster?
+// 2.2 further refine the movie posters:
+// change it so each movie poster is a <div> which has children for the poster <img>, the title <h2> and a short description <p>
+
+//second movie
+const RRR = document.createElement('img')
+RRR.src = "https://m.media-amazon.com/images/I/714lbsoabGL._AC_UF894,1000_QL80_.jpg"
+moviePosterTwo.append(RRR)
+
+const RRRTitle = document.createElement('h2')
+RRRTitle.textContent = "RRR"
+moviePosterTwo.prepend(RRRTitle)
+
+const RRRText = document.createElement('p')
+RRRText.textContent = "RRR is a really long movie. It might make you cry as well."
+moviePosterTwo.append(RRRText)
+
+
+//third movie
+const SixthSense = document.createElement('img')
+SixthSense.src = "https://www.masoncityposters.com/cdn/shop/products/The_Sixth_Sense_MCP0013_Movie_Poster_27x40_grande.jpg?v=1453534614"
+moviePosterThree.append(SixthSense)
+
+const SixthSenseTitle = document.createElement('h2')
+SixthSenseTitle.textContent = "Sixth Sense"
+moviePosterThree.prepend(SixthSenseTitle)
+
+const SixthSenseText = document.createElement('p')
+SixthSenseText.textContent = "Sixth Sense is a classic movie. You'll never guess the end!"
+moviePosterThree.append(SixthSenseText)
+
+// 4. console.log the title and description for the first movie poster
+console.log(marcelTitle, marcelText)
+
+// 5. change the <img> src, title text, and description text for the second movie poster
+RRR.src = 'https://static.wikia.nocookie.net/milkshake/images/6/6d/Boohbahs.jpg/revision/latest?cb=20200403103147'
+RRRTitle.textContent = "Boohbah"
+RRRText.textContent = "Boohbah is a weird show."
 
 // 6. delete the last movie poster
-
-
+SixthSense.remove()
 
 // // HINTS // //
 
 // given that adding movie posters is a repetetive task, it may be easier to build
 // a function that takes in certain arguments and does the work for you...
+
+function addMoviePosters(film, link, div) {
+    film = document.createElement('img')
+    film.src = link
+    div.append(film)  
+}
+
+addMoviePosters ("Gladiator", 
+                "https://statcdn.fandango.com/MPX/image/NBCU_Fandango/622/643/thumb_ACCD110B-A360-476A-A575-9B056EED316B.jpg", 
+                body)
+
 
 // remember that you need a few steps in order to finally see a dom element on the page:
 // create the element
